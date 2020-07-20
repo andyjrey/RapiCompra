db761679ff0fe4176d1b8bef46ae5a9c52bcd1c7
ANDREA <andyjrey@gmail.com>
2020-07-20 15:41:47 -0500

    Primera Version Activity Registro

--- app/src/main/java/com/uisrael/rapicompra/Productos.java
+++ app/src/main/java/com/uisrael/rapicompra/Productos.java
@@ -1,8 +1,22 @@
 package com.uisrael.rapicompra;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.internal.GoogleApiAvailabilityCache;
+
+import org.w3c.dom.Text;
 
 public class Productos extends AppCompatActivity {
 
@@ -11,4 +25,5 @@ public class Productos extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_productos);
     }
+
 }

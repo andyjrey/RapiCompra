68253fb507868863c5b281197fdd40b2af022ada
ANDREA <andyjrey@gmail.com>
2020-07-29 21:18:40 -0500

    Primera Version Activity Login

--- app/src/main/java/com/uisrael/rapicompra/Lista_Productos.java
+++ app/src/main/java/com/uisrael/rapicompra/Lista_Productos.java
@@ -2,7 +2,9 @@ package com.uisrael.rapicompra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Lista_Productos extends AppCompatActivity {
 
@@ -10,5 +12,26 @@ public class Lista_Productos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista__productos);
+    }
+
+
+    public void goTecScreen(View v) {
+        Intent intent = new Intent(this, Tecnologia.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+
+
+    }
+
+    public void goPreScreen(View v) {
+        Intent intent = new Intent(this, Prendas_Vestir.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    public void goVarioScreen(View v){
+        Intent intent=new Intent(this,Varios.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/uisrael/rapicompra/Prendas_Vestir.java
@@ -0,0 +1,14 @@
+package com.uisrael.rapicompra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Prendas_Vestir extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prendas__vestir);
+    }
+}

78f6038cbcfd355dcff3e64f30323f67a79a8861
ANDREA <andyjrey@gmail.com>
2020-08-02 17:26:43 -0500

    Primera Version Activity Pedido

--- app/src/main/java/com/uisrael/rapicompra/Prendas_Vestir.java
+++ app/src/main/java/com/uisrael/rapicompra/Prendas_Vestir.java
@@ -2,13 +2,52 @@ package com.uisrael.rapicompra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class Prendas_Vestir extends AppCompatActivity {
+    TextView preModa1,preModa2,preModa3;
+    CheckBox moda1,moda2,moda3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prendas__vestir);
+        preModa1=findViewById(R.id.txtPreModa);
+        preModa2=findViewById(R.id.txtPreModa2);
+        preModa3=findViewById(R.id.txtPreModa3);
+        moda1=findViewById(R.id.chkModa1);
+        moda2=findViewById(R.id.checkModa2);
+        moda3=findViewById(R.id.checkModa3);
     }
+
+    public void agregarPrendas(View v){
+        Intent intAgregarPedidoM=new Intent(Prendas_Vestir.this,Pedido.class);
+        if(moda1.isChecked()==true){
+            intAgregarPedidoM.putExtra("prenda1",moda1.getText().toString());
+            intAgregarPedidoM.putExtra("preModa1",preModa1.getText().toString());
+        }else
+        {
+            if(moda2.isChecked()==true){
+                intAgregarPedidoM.putExtra("prenda2",moda2.getText().toString());
+                intAgregarPedidoM.putExtra("preModa2",preModa2.getText().toString());
+            }
+            else{
+                intAgregarPedidoM.putExtra("prenda3",moda3.getText().toString());
+                intAgregarPedidoM.putExtra("preModa3",preModa3.getText().toString());
+            }
+        }
+
+        startActivity(intAgregarPedidoM);
+    }
+
+    public void Return(View v){
+        Intent intent=new Intent(this,Lista_Productos.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/uisrael/rapicompra/Tecnologia.java
+++ app/src/main/java/com/uisrael/rapicompra/Tecnologia.java
@@ -2,13 +2,49 @@ package com.uisrael.rapicompra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class Tecnologia extends AppCompatActivity {
+TextView  txtPre1, txtPre2, txtPre3;
+CheckBox  chBoP1,chBoC2,chBoT3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tecnologia);
+        txtPre1=findViewById(R.id.txtPreP);
+        txtPre2=findViewById(R.id.txtPreTel);
+        txtPre3=findViewById(R.id.txtPreTe);
+        chBoP1=findViewById(R.id.ckBoxPortatil);
+        chBoC2=findViewById(R.id.chkCell);
+        chBoT3=findViewById(R.id.chkBoxTe);
+    }
+
+    public void pasarDatos_Tec(View v){
+        Intent intAgregarPedidoT=new Intent(Tecnologia.this,Pedido.class);
+        if(chBoP1.isChecked()==true){
+            intAgregarPedidoT.putExtra("portatil",chBoP1.getText().toString());
+            intAgregarPedidoT.putExtra("preportatil",txtPre1.getText().toString());
+        }else {
+            if (chBoC2.isChecked() == true) {
+                intAgregarPedidoT.putExtra("telefono", chBoC2.getText().toString());
+                intAgregarPedidoT.putExtra("PreTel", txtPre2.getText().toString());
+            } else {
+                intAgregarPedidoT.putExtra("teclado", chBoT3.getText().toString());
+                intAgregarPedidoT.putExtra("preTeclado", txtPre3.getText().toString());
+            }
+        }
+        startActivity(intAgregarPedidoT);
+
+    }
+
+    public void Return(View v){
+        Intent intent=new Intent(this,Lista_Productos.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
